c2cf568b6bfae9482bfb10ea1d929685698b3d5d
Jakub <jakubowskijjal1@gmail.com>
2024-03-22 19:38:29 +0100

    feature: odejmowanie

--- main/java/com/example/javafx/Labs/no4/workingSpace.java
+++ main/java/com/example/javafx/Labs/no4/workingSpace.java
@@ -5,11 +5,18 @@ public class workingSpace
     public static void main(String[] args)
     {
         //test dzialania - kuba
-        System.out.println(dodawanie(5,3));
+        System.out.println(Dodawanie(5,3));
+        System.out.println(Odejmowanie(5,3));
     }
 
-    public static int dodawanie(int a, int b)
+    public static int Dodawanie(int a, int b)
     {
         return a+b;
     }
+
+    public static int Odejmowanie(int a, int b)
+    {
+        return a-b;
+    }
+
 }
