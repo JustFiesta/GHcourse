ab9d627db8c8635f544a633b61e82d9bb4e5d4f8
Jakub <jakubowskijjal1@gmail.com>
2024-03-22 19:37:45 +0100

    feature: dodawanie

---
+++ main/java/com/example/javafx/Labs/no4/workingSpace.java
@@ -0,0 +1,15 @@
+package com.example.javafx.Labs.no4;
+
+public class workingSpace
+{
+    public static void main(String[] args)
+    {
+        //test dzialania - kuba
+        System.out.println(dodawanie(5,3));
+    }
+
+    public static int dodawanie(int a, int b)
+    {
+        return a+b;
+    }
+}
