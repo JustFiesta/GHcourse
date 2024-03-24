92c6465f09ec2a520b0122ba6bcf1817185bebfb
bolson1313 <96063797+bolson1313@users.noreply.github.com>
2024-03-24 15:51:56 +0100

    feature: output changed

--- main/java/com/example/javafx/Labs/no4/feature.java
+++ main/java/com/example/javafx/Labs/no4/feature.java
@@ -2,6 +2,6 @@ package com.example.javafx.Labs.no4;
 
 public class feature {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Good night!");
     }
 }
