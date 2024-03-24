34be3fd5b28b4e2e75bd4db91d296079e9f4f02b
Jakub <jakubowskijjal1@gmail.com>
2024-03-24 18:34:12 +0100

    feature: new line

--- main/java/com/example/javafx/Labs/no4/fjuczer1.java
+++ main/java/com/example/javafx/Labs/no4/fjuczer1.java
@@ -3,5 +3,7 @@ package com.example.javafx.Labs.no4;
 public class fjuczer1 {
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        System.out.println("24.03.2024");
     }
 }
